0458534d9f747edd0677b06a01d18cb5fc71b6e8
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-12-18 09:30:06 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String cifrato = cifra("Il furly", 3);
+        String decifrato = cifra(cifrato, -3);
+
+        System.out.println("Cifrato: " + cifrato);
+        System.out.println("Decifrato: " + decifrato);
     }
 
-    
+    public static String cifra(String s, int k) {
+        String ret = "";
+
+        for(int i = 0; i < s.length(); ++i) {
+            int ascii = s.charAt(i);
+            char carCifrato = ' ';
+
+            if(ascii >= 65 && ascii <= 90) {
+                carCifrato = (char) (65 + ((ascii - 65 + k) % 26));
+            }
+            else if(ascii >= 97 && ascii <= 122){
+                carCifrato = (char) (97 + ((ascii - 97 + k) % 26));
+            }
+            ret += carCifrato;
+        }
+
+        return ret;
+    }
 }
